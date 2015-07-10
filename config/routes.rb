a27819b33539d33b74b199efa357d4355b702851
smoll <smollah@theorchard.com>
2015-07-10 12:50:43 -0400

    Setup for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  # Define a root_url for Devise
+  root "bookmarks#index"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
