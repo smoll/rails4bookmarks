2e4ce803b4fa37c76abec1f3f20e59658b904692
smoll <smollah@theorchard.com>
2015-07-10 12:59:17 -0400

    Generate User model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :omniauthable
+  # :recoverable, and :trackable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :validatable
+end
