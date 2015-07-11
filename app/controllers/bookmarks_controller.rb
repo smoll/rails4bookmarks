91c2535b15bd4c2fd04fce51028dbc5e43b2e2ee
Shujon Mollah <smollah@theorchard.com>
2015-07-11 00:57:36 -0400

    Add alert when invalid bookmark url is navigated to

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -68,7 +68,9 @@ class BookmarksController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_bookmark
-    @bookmark = current_user.bookmarks.find(params[:id])
+    @bookmark = current_user.bookmarks.find_by(id: params[:id])
+    return @bookmark if @bookmark
+    redirect_to root_url, alert: "Bookmark not found."
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
